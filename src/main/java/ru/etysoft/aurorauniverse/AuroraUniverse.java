d9b3acf5d68ffd49950bca4831d7eb1796b23922
karlovm <etysoft@yandex.ru>
2020-05-24 09:11:22 +0300

    Cloud

---
+++ src/main/java/ru/etysoft/aurorauniverse/AuroraUniverse.java
@@ -0,0 +1,17 @@
+package ru.etysoft.aurorauniverse;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AuroraUniverse extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic1фывыфв
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
