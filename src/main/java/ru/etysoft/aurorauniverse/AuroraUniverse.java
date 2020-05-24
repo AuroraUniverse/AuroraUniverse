8940072ab95c1d9a84f42f101c7d3d82fc68ee9b
karlovm <etysoft@yandex.ru>
2020-05-24 09:28:10 +0300

    First

--- src/main/java/ru/etysoft/aurorauniverse/AuroraUniverse.java
+++ src/main/java/ru/etysoft/aurorauniverse/AuroraUniverse.java
@@ -6,7 +6,7 @@ public final class AuroraUniverse extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic1фывыфв
+        // Plugin startup logic1фывыфвg
 
     }
 
